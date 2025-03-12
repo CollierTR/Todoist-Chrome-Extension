8edf999d0bf15d4255a407548656255b47d35e63
CollierTR <80648892+CollierTR@users.noreply.github.com>
2025-03-11 23:25:39 -0500

    first commit

---
+++ content-script.js
@@ -0,0 +1,30 @@
+
+console.log("start of script")
+
+//  class=  fc-timegrid-slot    fc-timegrid-slot-lane   fc-timegrid-slot-minor
+
+setTimeout(injectColor, 1000)
+
+function injectColor() {
+    let timeSlots = document.querySelectorAll('.fc-timegrid-slot-lane')
+
+    timeSlots.forEach((slot, index) => {
+        console.log(index)
+        if (index > 0 && index < 20 || index > 87 && index < 96) { // 1am-5am & 10pm-12pm
+            slot.classList.add('night')
+        }
+        if (index > 27 && index < 64) {
+            slot.classList.add('work')
+        }
+        if (index >= 20 && index <= 27 || index >= 64 && index <= 67) {
+            slot.classList.add('golden-hour')
+        }
+        if ((index + 1) % 4 ===0) {
+            slot.classList.add('border')
+        }
+
+    })
+
+}
+
+console.log("end of script")
