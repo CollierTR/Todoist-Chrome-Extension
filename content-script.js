a0054e1aa470a73f59b842bfb8ac83a38d0bde29
CollierTR <80648892+CollierTR@users.noreply.github.com>
2025-03-12 20:41:51 -0500

    Corrected the size of the favicon | Added more colors categories

--- content-script.js
+++ content-script.js
@@ -19,6 +19,12 @@ function injectColor() {
         if (index >= 20 && index <= 27 || index >= 64 && index <= 67) {
             slot.classList.add('golden-hour')
         }
+        if (index >= 68 && index <= 75) {
+            slot.classList.add('family-time')
+        }
+        if (index >= 76 && index <= 87) {
+            slot.classList.add('cyber-focus')
+        }
         if ((index + 1) % 4 ===0) {
             slot.classList.add('border')
         }
